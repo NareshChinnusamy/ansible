4154b1e059833ed7e9e3c119a79f03e1ea0b0002
Naresh <naresh@breezeware.net>
2023-01-09 18:52:17 +0530

    Updated docker setup for ubuntu

--- docker-permission.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "Entering from ansible" >> /tmp/ansible.txt
-systemctl enable docker
-systemctl start docker
-systemctl status docker >> /tmp/ansible.txt
-echo "Leaving from asnible" >> /tmp/ansible.txt
